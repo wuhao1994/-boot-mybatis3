82e0bcd6055952daa070a37b16acdb995cb273e0
wuhao1994 <justdowh@126.com>
2018-01-03 23:22:10 +0800

    rabbitmq

--- boot-mabatis/src/test/java/com/example/demo/BootMabatisApplicationTests.java
+++ boot-mabatis/src/test/java/com/example/demo/BootMabatisApplicationTests.java
@@ -2,8 +2,11 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.rabbit.HelloSender;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,5 +16,12 @@ public class BootMabatisApplicationTests {
 	public void contextLoads() {
 		System.out.println("hello world");
 	}
+	@Autowired
+    private HelloSender helloSender;
+
+    @Test
+    public void hello() throws Exception {
+        helloSender.send();
+    }
 
 }
